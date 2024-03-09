91902a02f3985207dd8798309922b5a1982739a1
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2024-03-09 15:57:33 +0200

    dadada

--- First Year/Algorithms and Data Structures - C++/26_02_2024/26_02_2024.cpp
+++ First Year/Algorithms and Data Structures - C++/26_02_2024/26_02_2024.cpp
@@ -1,20 +1,42 @@
-// 26_02_2024.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ * 5  5  5  5  5
+ * 5  4  4  4  5
+ * 5  4  3  4  5
+ * 5  4  4  4  5
+ * 5  5  5  5  5
+ *
+ */
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "bg");
+	const int N = 5;
+	int i, j;
+	int a[N][N];
+
+
+	for (j = 0; j <= N / 2; j++)
+		for (i = j; i < N - j; i++)
+		{
+			a[j][i] = N - j;
+			a[i][N - 1 - j] = N - j;
+			a[N - 1 - j][i] = N - j;
+			a[i][j] = N - j;
+		}
+
+	//print
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+
+			cout << a[i][j] << "\t";
+
+		cout << "\n";
+	}
+	system("pause");
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- First Year/Algorithms and Data Structures - C++/26_02_24/26_02_24.cpp
+++ First Year/Algorithms and Data Structures - C++/26_02_24/26_02_24.cpp
@@ -1,67 +1,44 @@
-// 26_02_24.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ * 5  5  5  5  5
+ * 0  4  4  4  4
+ * 0  0  3  3  3
+ * 0  0  0  2  2
+ * 0  0  0  0  1
+ *
+ */
 
 #include <iostream>
 using namespace std;
-int main() {
-    const int rows = 6;  
-    const int cols = 6;  
 
-    int array[rows][cols];
+int main(int arg, char* argv[])
+{
+	setlocale(LC_ALL, "bg");
+	const int N = 5;
+	int i, j;
+	int a[N][N];
 
-    
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (i == j) {
-                array[i][j] = 0;  
-            }
-            else {
-                
-                array[i][j] = abs(i - j);
-            }
-        }
-    }
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			a[i][j] = 0;
+
+	for (i = 0; i < N; i++)
+		for (j = i; j < N; j++)
+		{
+			a[i][j] = N - i;
+
+		}
+
+	//print
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+
+			cout << a[i][j] << "\t";
+
+		cout << "\n";
+	}
+	system("pause");
+	return 0;
 }
 
-//int main(){
-//int matrix[5][5];
-//int marker = 1;
-//
-//fo
-//for (int i = 1; i < 5; i++)
-//
-//	
-//
-//for (int i = 0; i < 5; i++)
-//{
-//	for (int j = 0; j < 5; j++)
-//	{
-//
-//		if (i == j)
-//		{
-//			matrix[i][j] = 0;
-//		}
-//
-//	}
-//
-//}
-//for (int i = 0; i < 5; i++)
-//{
-//	for (int j = 0; j < 5; j++)
-//	{
-//		cout << "\t" << matrix[i][j];
-//	}
-//	cout << endl;
-//}
-//}
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
