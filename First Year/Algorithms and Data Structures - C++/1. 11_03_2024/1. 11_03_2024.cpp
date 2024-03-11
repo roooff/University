6ed4c4353659522810fc2ac9ec1f7e190dfa1a1c
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2024-03-11 12:15:04 +0200

    fdfdfdfd

--- First Year/Algorithms and Data Structures - C++/1. 11_03_2024/1. 11_03_2024.cpp
+++ First Year/Algorithms and Data Structures - C++/1. 11_03_2024/1. 11_03_2024.cpp
@@ -12,35 +12,33 @@ int main()
     int b[5][5];
     int c[5][5];
     int d[5][5];
-    for (int i = 0; i < 5; i++)
-    {
-        for ( int j = 0; j < 5; j++)
-
-            cout << a[i][j] << "\t";
-
-        cout << "\n";
-    }
+    
     //Inverting the matrix
     // 14,13,12,11,10
     // 25,24,23,22,21
     // 30,29,28,27,26
     // 35,34,33,32,31
     // 40,39,38,37,36
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            // Swap elements in each row
-            int temp = a[i][j];
-            a[i][j] = a[i][4 - j];
-            a[i][4 - j] = temp;
-        }
-    }
+    
+    //Inverting the matrix :
+    // 36,37,38,39,40
+    // 31,32,33,34,35
+    // 26,27,28,29,30
+    // 21,22,23,24,25
+    // 10,11,12,13,14
+
+    //Inverting the matrix:
+    // 40, 39, 38, 37, 36
+    // 35, 34, 33, 32, 31
+    // 30, 29, 28, 27, 26
+    // 25, 24, 23, 22, 21
+    // 14, 13, 12, 11, 10
+
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
 
-            cout << b[i][j] << "\t";
+            cout << a[i][j] << "\t";
 
         cout << "\n";
     }
-}
-
