92812aa6acf21cd3502e158f9695b17715c2b60a
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2024-03-18 11:49:32 +0200

    Adding

--- First Year/Algorithms and Data Structures - C++/1. 18_03_2024/1. 18_03_2024.cpp
+++ First Year/Algorithms and Data Structures - C++/1. 18_03_2024/1. 18_03_2024.cpp
@@ -1,20 +1,19 @@
-// 1. 18_03_2024.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+// recursion example: 
+using namespace std;
+void rec(int i) {
+	cout << i << " ";
+	if (i > 0)
+	{
+		rec(i - 1);
 
+	}
+	cout <<i<<" ";
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	rec(100000000000000000);
+	return(0);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
